0a76eed300f35a93e3f9f16bf3f28830ce6020d1
vishnu <vishnu0908@gmail.com>
2023-10-24 18:08:20 -0700

    SSH

--- utils/utilities.py
+++ utils/utilities.py
@@ -1,31 +1,48 @@
-import os
 import json
+import os
 
-class Utilities:
-    """
-    Utility class containing static methods for various utility functions.
-    """
+
+class Utility:
+
     @staticmethod
-    def read_tags_from_file(filepath):
+    def read_tags_from_file(file_path):
         """
-        Reads the tag values from the given file.
+        Reads a JSON file containing tags and returns a list of tag dictionaries.
 
-        :param filepath: The path to the file containing the tag values.
-        :return: A list of tag values.
+        :param file_path: Path to the JSON file containing tags.
+        :return: List of tag dictionaries.
         """
-        if not os.path.exists(filepath):
-            raise FileNotFoundError(f"File {filepath} not found.")
+        if not os.path.exists(file_path):
+            raise ValueError(f"The provided file path {file_path} does not exist.")
 
-        with open(filepath, 'r') as file:
+        with open(file_path, 'r') as file:
             tags = json.load(file)
+
+        if not isinstance(tags, list):
+            raise ValueError("The provided JSON file does not contain a list of tags.")
 
         return tags
 
-    # Additional utility static methods can be added below.
+    @staticmethod
+    def generate_output_path(account, region, az, date):
+        """
+        Generates an output path based on provided parameters.
 
+        :param account: AWS account.
+        :param region: AWS region.
+        :param az: AWS availability zone.
+        :param date: Current date in 'YYYY-MM-DD' format.
+        :return: Output path string.
+        """
+        return os.path.join("outputs", account, region, az, date)
 
-# Usage Example
-if __name__ == "__main__":
-    tag_filepath = "path_to_tags_file.json"
-    tags = Utilities.read_tags_from_file(tag_filepath)
-    print(tags)
+    @staticmethod
+    def check_file_directory(file_path):
+        """
+        Checks if the directory for the given file path exists, if not, creates it.
+
+        :param file_path: Path to the file.
+        """
+        directory = os.path.dirname(file_path)
+        if directory and not os.path.exists(directory):
+            os.makedirs(directory)
